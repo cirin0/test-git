090f54cf4437f6a1b73ca1802a504403b49e6f2a
cirin0 <ivan.gruziv@gmail.com>
2025-02-03 19:58:06 +0200

    add User model class with JPA annotations

---
+++ src/main/java/org/flow/testgit/model/User.java
@@ -0,0 +1,14 @@
+package org.flow.testgit.model;
+
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "users")
+@Data
+public class User {
+  private Long id;
+  private String name;
+  private String email;
+  private String password;
+  private String role;
+}
