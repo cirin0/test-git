c853e3040f0479246de1f5a8efe9724fa701c264
cirin0 <ivan.gruziv@gmail.com>
2025-02-05 15:51:17 +0200

    refactor UserService: update package structure and add user management methods

--- src/main/java/org/flow/testgit/service/UserService.java
+++ src/main/java/org/flow/testgit/service/UserService.java
@@ -1,5 +1,8 @@
-package org.flow.testgit;
+package org.flow.testgit.service;
 
+
+import org.flow.testgit.model.User;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
@@ -10,4 +13,19 @@ public class UserService {
 	 public void deleteUser(Long id) {
 		  // some logic
 	 }
+
+  public void updateUser(User user) {
+    // some logic
+  }
+
+  public User getUserById(Long id) {
+    // some logic
+    return new User();
+  }
+
+  public User getUserByEmail(String email) {
+    // some logic
+    return new User();
+  }
+
 }
